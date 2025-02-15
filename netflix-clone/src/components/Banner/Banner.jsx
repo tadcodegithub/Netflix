b052d7633da19c7039d74d31b63e80be6030fe12
Your GitHub Username <tadess1127@gmail.com>
2025-02-15 03:35:08 +0300

    fetch sample movies data and set on state

--- netflix-clone/src/components/Banner/Banner.jsx
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -6,13 +6,28 @@ import styles from "./banner.module.css";
 
 function Banner() {
   //check the connection of urls and my api
-  console.log(requests.fetchNetflixOriginal);
- 
+  // console.log(requests.fetchNetflixOriginal);
+  //creating state with the name of movies and setMovies updater
+  const [movies, setMovies] = useState({});
+  useEffect(() => {
+    async function getmovies() {
+      const response = await axios.get(requests.fetchNetflixOriginal);
+      //check the response data
+      console.log(response.data.results);
+      setMovies(response.data.results);
+    }
+    getmovies();
+  },[]);
 
   return (
     <>
-      <div>his is banner place holder</div>
-</>
+      <div
+        className={`${styles.banner_img} `}
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original${movies?.backdrop_path}`,
+        }}
+      ></div>
+    </>
   );
 }
 
